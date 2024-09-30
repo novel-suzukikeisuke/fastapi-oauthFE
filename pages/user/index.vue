fc11b0e13c63a4867abd907d679d0be7a2058462
suzuki <keisuke.suzuki@novel.jpn.com>
2024-09-30 15:19:18 +0900

    テスト用画面作成

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <p>ユーザー一覧</p>
+  <v-card
+    class="mx-auto"
+    width="400"
+  >
+    <v-card-title class="text-h5">ユーザー一覧</v-card-title>
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        block
+        class="mt-4"
+        @click="logOut"
+      >
+        ログアウト
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
+import { useAuth } from '~/composables/useAuth';
 
+// ログアウト関数の呼び出し
+const { logOut } = useAuth();
 </script>
 
 <style scoped>
