5ea54b9151f34daea4278e87b9e28b05e6fa4834
suzuki <keisuke.suzuki@novel.jpn.com>
2024-09-30 16:23:25 +0900

    共通レイアウト作成

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fluid style="height: 100vh;">
+    <v-row
+      style="height: 100%;"
+    >
+      <v-col>
+        <main>
+          <slot />
+        </main>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
