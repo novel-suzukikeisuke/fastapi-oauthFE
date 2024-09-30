8c53996d083186fd9924c307117a4e3a7572b6e7
suzuki <keisuke.suzuki@novel.jpn.com>
2024-09-30 14:30:18 +0900

    ログイン関数作成

---
+++ composables/useAuth.ts
@@ -0,0 +1,35 @@
+import { useAuthStore } from '~/store/auth';
+import { apiBaseUrl } from '~/config';
+import type { AuthResponse } from '~/types/auth';
+
+export const useAuth = () => {
+  const authStore = useAuthStore();
+
+  const login = async (username: string, password: string) => {
+    try {
+      const response = await fetch(`${apiBaseUrl}/api/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          'username': username,
+          'password': password,
+        })
+      })
+      const data: AuthResponse = await response.json()
+      if (response.ok) {
+        authStore.setToken(data.access_token);
+        navigateTo('/user')
+      } else {
+        alert(data.error)
+      }
+    } catch (error) {
+      console.error('An error occurred:', error)
+    }
+  }
+
+  return {
+    login
+  };
+};
