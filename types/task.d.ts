82f5831f041ba82409648a21368303fc9b263dbc
suzuki <keisuke.suzuki@novel.jpn.com>
2024-10-01 16:20:01 +0900

    タスク型定義修正

--- types/task.d.ts
+++ types/task.d.ts
@@ -1,9 +1,11 @@
+import type { TagResponse } from '~/types/tag';
+
 export type TaskResponse = {
   id: number;
   title: string;
   description: string;
   completed: boolean;
   user_id: number;
-  tags: string[];
+  tags: TagResponse[];
   error?: string;
 };
