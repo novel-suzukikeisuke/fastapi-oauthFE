35938c1c095df3fea11dec355df139e4552caccf
suzuki <keisuke.suzuki@novel.jpn.com>
2024-10-03 16:13:48 +0900

    登録関数実装

--- composables/useTag.ts
+++ composables/useTag.ts
@@ -7,6 +7,32 @@ export const useTag = () => {
   const tags = ref<TagResponse[]>([]);
   const authStore = useAuthStore();
 
+  const createTag = async (name: string, color: number) => {
+    authStore.loadToken();
+    try {
+      const response = await fetch(`${apiBaseUrl}/api/tags/create`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${authStore.token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          color: color
+        })
+      })
+      const data: TagResponse = await response.json()
+      if (response.ok) {
+        return true;
+      } else  {
+        alert(data.detail)
+        return false;
+      }
+    } catch (error) {
+      console.error('An error occurred:', error)
+    }
+  }
+
   const fetchTags = async () => {
     authStore.loadToken();
     try {
@@ -19,7 +45,7 @@ export const useTag = () => {
       if (response.ok) {
         tags.value = data;
       } else {
-        alert(data[0]?.error || '不明なエラーが発生しました');
+        alert(data[0]?.detail || '不明なエラーが発生しました');
       }
     } catch (err) {
       console.error('An error occurred:', err);
@@ -28,6 +54,7 @@ export const useTag = () => {
   };
 
   return {
+    createTag,
     fetchTags,
     tags
   };
