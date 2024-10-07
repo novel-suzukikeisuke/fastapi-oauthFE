d6ea5e873a4f9038814b1649f6770809544b2d9e
suzuki <keisuke.suzuki@novel.jpn.com>
2024-10-07 10:53:44 +0900

    タグフィルター修正

--- components/tasks/filterTag.vue
+++ components/tasks/filterTag.vue
@@ -1,38 +1,57 @@
 <template>
-  <v-select
-    v-model="selectedTagId"
-    :items="tags"
-    label="関連タグ"
-    item-title="name"
-    item-value="id"
-    @change="onFilterChange"
-  />
+  <v-dialog v-model="isActive" max-width="500">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      v-bind="activatorProps"
+      color="surface-variant"
+      text="タグ"
+      variant="flat"
+      width="100"
+      @click="fetchTags"
+    ></v-btn>
+  </template>
+
+  <template v-slot:default>
+    <v-card title="タグ絞り込み">
+      <v-card-item>
+        <v-select
+          label="タグを選択してください"
+          item-title="name"
+          item-value="id"
+          :items="tags"
+          v-model="selectedTagId"
+        ></v-select>
+      </v-card-item>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          text="検索"
+          :disabled="!selectedTagId"
+          @click="applyFilter"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { TagResponse } from '~/types/tag';
+const { fetchTags, tags } = useTag();
+const { filterTagId } = useTask();
 
-const props = defineProps({
-  tags: {
-    type: Array as () => TagResponse[], // ここでTagResponseを使用
-    required: true,
-  },
-  filterTagId: {
-    type: Number,
-    default: null,
-  },
-});
+const emit = defineEmits(['taskFetch']);
 
-const emit = defineEmits(['filterChange']);
+const isActive = ref<boolean>(false);
+const selectedTagId = ref<number | null>(null);
 
-const selectedTagId = ref(props.filterTagId);
-
-const onFilterChange = () => {
-  emit('filterChange', selectedTagId.value);
+const applyFilter = () => {
+  filterTagId.value = selectedTagId.value;
+  isActive.value = false;
+  selectedTagId.value = null;
+  emit('taskFetch', filterTagId.value);
 };
 
-watch(selectedTagId, (newValue) => {
-  emit('filterChange', newValue); // 新しい値をフィルタリングに渡す
-});
 </script>
+
+<style scoped>
+</style>
