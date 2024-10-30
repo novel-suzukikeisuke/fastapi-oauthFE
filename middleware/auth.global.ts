4e1d7d08f8bf32ea4698241c76bff146e08dc1df
suzuki <keisuke.suzuki@novel.jpn.com>
2024-10-30 15:44:08 +0900

    各リダイレクト条件確認、追加

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -13,18 +13,28 @@ export default defineNuxtRouteMiddleware(async (to) => {
 
     // トークンが存在しない場合はログインページへリダイレクト
     if (!authStore.token && !NOT_AUTH_PAGES.includes(to.path)) {
-      setTimeout(() => {
-        return navigateTo('/', { replace: true })
-      }, 100) // 遷移後レイアウトが崩れるため100ms程度の遅延を追加
+      setTimeout(() => navigateTo('/', { replace: true }), 100) // 遷移後レイアウトが崩れるため100ms程度の遅延を追加
+      return
     }
 
     if (authStore.token) {
-      const user = await fetchUser()
+      try {
+        const user = await fetchUser()
 
-      if (user) {
-        if (to.path === '/users' && user.role !== UserRole.ADMIN) {
-          return navigateTo('/tasks')
+        // 認証済みのユーザーがNOT_AUTH_PAGESにアクセスした場合は/tasksにリダイレクト
+        if (NOT_AUTH_PAGES.includes(to.path)) {
+          return navigateTo('/tasks', { replace: true })
         }
+
+        // ユーザーがADMINでない場合は/usersページへのアクセスを制限
+        if (user && to.path === '/users' && user.role !== UserRole.ADMIN) {
+          return navigateTo('/tasks', { replace: true })
+        }
+      }
+      catch {
+        // トークンが無効または取得に失敗した場合、トークンを削除してログインページへリダイレクト
+        authStore.logout()
+        return navigateTo('/', { replace: true })
       }
     }
   }
