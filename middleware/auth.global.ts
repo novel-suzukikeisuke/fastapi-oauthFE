31adf5ca5d5b8279ee0004c5c6fbdc76948e4197
suzuki <keisuke.suzuki@novel.jpn.com>
2024-10-31 10:09:12 +0900

    import.meta.clientを削除

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -8,34 +8,31 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const NOT_AUTH_PAGES = ['/signUp', '/']
 
   // クライアントサイドのみでトークンをロード
-  if (import.meta.client) {
-    await authStore.loadToken()
+  await authStore.loadToken()
 
-    // トークンが存在しない場合はログインページへリダイレクト
-    if (!authStore.token && !NOT_AUTH_PAGES.includes(to.path)) {
-      setTimeout(() => navigateTo('/', { replace: true }), 100) // 遷移後レイアウトが崩れるため100ms程度の遅延を追加
-      return
+  // トークンが存在しない場合はログインページへリダイレクト
+  if (!authStore.token && !NOT_AUTH_PAGES.includes(to.path)) {
+    return navigateTo('/', { replace: true })
+  }
+
+  if (authStore.token) {
+    try {
+      const user = await fetchUser()
+
+      // 認証済みのユーザーがNOT_AUTH_PAGESにアクセスした場合は/tasksにリダイレクト
+      if (NOT_AUTH_PAGES.includes(to.path)) {
+        return navigateTo('/tasks', { replace: true })
+      }
+
+      // ユーザーがADMINでない場合は/usersページへのアクセスを制限
+      if (user && to.path === '/users' && user.role !== UserRole.ADMIN) {
+        return navigateTo('/tasks', { replace: true })
+      }
     }
-
-    if (authStore.token) {
-      try {
-        const user = await fetchUser()
-
-        // 認証済みのユーザーがNOT_AUTH_PAGESにアクセスした場合は/tasksにリダイレクト
-        if (NOT_AUTH_PAGES.includes(to.path)) {
-          return navigateTo('/tasks', { replace: true })
-        }
-
-        // ユーザーがADMINでない場合は/usersページへのアクセスを制限
-        if (user && to.path === '/users' && user.role !== UserRole.ADMIN) {
-          return navigateTo('/tasks', { replace: true })
-        }
-      }
-      catch {
-        // トークンが無効または取得に失敗した場合、トークンを削除してログインページへリダイレクト
-        authStore.logout()
-        return navigateTo('/', { replace: true })
-      }
+    catch {
+      // トークンが無効または取得に失敗した場合、トークンを削除してログインページへリダイレクト
+      authStore.logout()
+      return navigateTo('/', { replace: true })
     }
   }
 })
